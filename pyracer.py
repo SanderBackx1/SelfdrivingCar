a145c07c1f8a68db5f77cd66375f23e9c233499e
SanderBackx1 <sander.backx123@gmail.com>
2020-12-01 10:20:59 +0100

    keyboard

--- pyracer.py
+++ pyracer.py
@@ -4,20 +4,79 @@ import cv2
 import time
 from PIL import ImageGrab
 import numpy as np
-
+import keyboard
 
 j = pyvjoy.VJoyDevice(1)
 x_max = 32767
 
+def crop(img):
+    x=0
+    w=1600
+    y=350
+    h=900-350
+
+    return img[y:y+h, x:x+w]
+        
+
 def sendInputs(x,z):
+    if  x >= 0.51:
+        x += 0.2
+    elif x<=0.48:
+        x -= 0.1
     print(f'X:{x*(x_max+x_max)-x_max} Z {z*512 - 255}')
+
     j.data.wAxisX = int(x*x_max)
-    j.data.wAxisZ=int(z*x_max)
+    j.data.wAxisZ=int( (z*x_max) * 2 )
 
     j.data.wAxisY= int(0)
     #send data to vJoy device
     j.update()
-    
+
+
+def inputModel(steer, accel,brake):
+    print(f'X:{steer} accel {accel} Brake:{brake}')
+    #unnormalizing
+
+    #x_max = 32767
+    x_half = int(x_max/2)
+
+
+    # -255 255
+    #0 - 1 
+    #<0.5  0.5>
+    # 
+
+
+    s  = steer * 32767
+    a = accel * x_half 
+    b = brake * x_half * -1
+    j.data.wAxisX = int(s)
+    j.data.wAxisZ=int( a + b)
+
+    j.data.wAxisY= int(0)
+    #send data to vJoy device
+    j.update()
+
+
+
+
+
+def resetkey():
+    j.data.wAxisX = int(x_max/2)
+    j.data.wAxisZ=int(x_max/2)
+
+    j.data.wAxisY= int(x_max/2)
+    #send data to vJoy device
+    j.update()
+
+
+
+def listenkeys():
+    keys = ['left', 'up', 'right']    
+    for k in lab_dictkey:
+        keys[keys.index(k)] = keyboard.is_pressed(k)
+        print("manual steering: "+str(keys))
+        
 def run():
 
     autopilot = False
@@ -30,10 +89,12 @@ def run():
         # printscreen = np.array(ImageGrab.grab(bbox=(0, 80, 1024, 700)))
         # resized = cv2.resize(printscreen, (244,244))
         # cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+     
         resolutionx, resolutiony = (1600,900)
         margin = 40
         printscreen = np.array(ImageGrab.grab(bbox=(0, margin, resolutionx, resolutiony+margin)))
         printscreen = cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB)
+        printscreen = crop(printscreen)
         resized = cv2.resize(printscreen, (480,277))
         smoll = cv2.resize(printscreen, (int(resolutionx/2),int(resolutiony/2)))
         cv2.imshow('window', resized)
@@ -50,13 +111,15 @@ def run():
         
 
         k = cv2.waitKey(25)
-        if  k == ord('q'):
+        if  k == ord('g') or keyboard.is_pressed('g'):
             cv2.destroyAllWindows()
             break
-        elif k == ord('d'):
-            time.sleep(3)
+        elif k == ord('v') or keyboard.is_pressed('v') :
+            resetkey()
             autopilot = not autopilot
-        elif k == ord('f'):
+            print('autopilot in 5s')
+            time.sleep(5)
+        elif k == ord('f') or keyboard.is_pressed('f'):
             showFps = not showFps
        
 
