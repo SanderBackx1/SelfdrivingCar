2e1bb6e7514ba901604ad6aac5c026258e74ebdb
SanderBackx1 <sander.backx123@gmail.com>
2020-12-01 11:33:27 +0100

    added rdp to model

--- racemodel.py
+++ racemodel.py
@@ -10,6 +10,7 @@ images = [ f"./data/images/{x}" for x in os.listdir("./data/images")]
 #get input data
 df_data = pd.read_csv('./data/inputs.csv',names=["img_path", "X", "Y", "Throttle", "Brake"])
 
+
 #We don't need the input of the Y axis because this doesn't influence the steering of the vehicle
 del df_data["Y"]
 
@@ -50,6 +51,17 @@ def get_x (r): return Path(f"./data/images/{r['img_path']}")
 def get_y(r): return [r['X'], r['Z']]
 
 
+##RDP
+df_rdp = pd.read_csv('./data/inputs2.csv',names=["img_path", "speed", "steer", "accel", "brake"])
+df_rdp["steer"] = df_rdp["steer"].transform(lambda x: '0' if x == "None" else x)
+df_rdp['steer'] = df_rdp['steer'].apply(lambda x: (float(x)+255)/512)
+df_rdp['accel'] = df_rdp['accel'].apply(lambda x: float(x)/255)
+df_rdp['brake'] = df_rdp['brake'].apply(lambda x: float(x)/255)
+def get_y_rdp(r): return [r['steer'], r['accel'], r['brake']]
+
+
+
+
 datablock = DataBlock(
                 blocks=(ImageBlock,RegressionBlock),
                 get_x=get_x,
@@ -57,9 +69,9 @@ datablock = DataBlock(
                 item_tfms=Resize((277,480), method='squish'),
                 splitter=RandomSplitter(valid_pct=0.2, seed=32))
 
-dls = datablock.dataloaders(df_final_removed)
+dls = datablock.dataloaders(df_data)
 learn = cnn_learner(dls, resnet50, y_range=(0,1), metrics=mse )
-learn = learn.load('racecar50epochs')
+learn = learn.load('roimodel23')
 
 def getOutput(img):
     return learn.predict(img)
