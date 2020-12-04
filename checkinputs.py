f3c0d618f688746367dba3c7f4ca604765e38949
Yori Verbist <yori.verbist1212@gmail.com>
2020-12-04 20:23:35 +0100

    Script to remove data where steering = 0

--- checkinputs.py
+++ checkinputs.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 
 
-with open('data/inputs.csv', 'r') as file:
+with open('data/inputs2.csv', 'r') as file:
     inputs = csv.DictReader(file)
     steer = [int(x['steer']) for x in inputs]
     #print(steer)
